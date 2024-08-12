2c655647973265060c2c8921ad084e7a7d45f23c
Kuldip <kuldipsathvara2508@gmail.com>
2024-08-12 17:40:25 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Music Academy
+
+- This project is created while learning nextjs from NextJS music academy project(Chai aur code - Hitesh Choudhary)
+
+- I used aceternity ui components to make ui
+
+## Getting Started
+
+```js
+npm i
+npm run dev
+```
+
+Project will start on localhost:3000 port.
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
+}
